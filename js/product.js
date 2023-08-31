ae9b39b89404478d3048f6b4f3a1ced59d977906
Ellen <elre0001@stud.kea.dk>
2023-08-31 09:59:25 +0200

    Side

---
+++ js/product.js
@@ -0,0 +1,15 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  document.querySelector(".purchaseBox h3").textContent =
+    product.productdisplayname;
+  document.querySelector(".purchaseBox .brand").textContent = product.brandname;
+  document.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}
---
+++ js/productlist.js
@@ -0,0 +1,26 @@
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("category");
+
+fetch("https://kea-alt-del.dk/t7/api/products?category=" + category)
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  const template = document.querySelector("#smallProductTemplate").content;
+  const copy = template.cloneNode(true);
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  copy
+    .querySelector(".read-more")
+    .setAttribute("href", `product.html?id=${product.id}`);
+  copy.querySelector(
+    "img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+  document.querySelector("main").appendChild(copy);
+}
